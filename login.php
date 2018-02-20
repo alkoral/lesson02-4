21ee442ce6a374a398c99a5153e934de96a3fbe8
alkoral <35643632+alkoral@users.noreply.github.com>
2018-02-20 23:43:06 +0300

    Delete login.php

--- login.php
+++
@@ -1,7 +0,0 @@
-<?php echo ($_POST['userName']) ?>
-
-<!--
-	https://github.com/icron/netology/tree/master/php/flow20/lesson8
-	https://netology-university.bitbucket.io/php/homework/2.4-session/
-
--->
